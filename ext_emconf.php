12a7f9a18d55723effd99c5b0e3e786b12ecc7c9
Peter Schöne <ps@yuniq.lu>
2025-06-27 09:12:06 +0200

    dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,9 +3,14 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'feuser_newsletter_subscription',
     'description' => '(Un-)subscribe-form to tx_mail_newsletter in fe_users',
+    'author' => 'Peter Schöne',
+    'author_email' => 'dev@peterschoene.de',
+    'state' => 'stable',
+    'version' => '1.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-12.4.99',
+            'mail' => '2.1.0-3.99.99'
         ],
     ],
     'autoload' => [
